9f0cdbedf69d0081eb8d86f92bfbf848eae305a1
MTA-MAC <mta-mac@MTA-MACs-iMac-4.local>
2020-03-10 14:37:46 +0700

    การบ้านข้อ 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,39 +1,48 @@
 import UIKit
 
 var str = "Hello playground"
-print ("Hello")
-2+6
-4-3
-2*2
-8/4
-(28.0).truncatingRemainder(dividingBy: 10.0)
-(28.1).truncatingRemainder(dividingBy: 10.0)
-sin(45*Double.pi/180)
-let number:Int = 10
-let pi: Double = 3.14159
-var variableNumber: Int = 42
-variableNumber = 0
-variableNumber = 1_000_000
-var counter: Int = 0
-counter += 1
-counter -= 1
-let testnumber = 45
-let evenodd = testnumber % 2
-print(evenodd)
-let hourlyrate:Double=19.5
-let hourworked:Int=10
-let totalCost:Double=hourlyrate*Double(hourworked)
-var massage="Hello"+"My name is"
-var name="Matt"
-massage += name
-let oneThird=1.0/3.0
-let oneThirdLongString="One Third is \(oneThird)as a demical"
-let coordinate=(x:2.1,y:3.2)
-let x1=coordinate.x
-let y1=coordinate.y
-let a=5
-let b=10
-let min=a<b ? a:b
-let max=a>b ? a:b
-print(max)
-print(min)
+
+var InputNumber1 : Double = 5
+var InputNumber2 : Double = 4
+var Summary = (InputNumber1+InputNumber2)
+var Different = (InputNumber1-InputNumber2)
+var Multiply = (InputNumber1*InputNumber2)
+var Divide = (InputNumber1/InputNumber2)
+
+print ("Summary =",Summary)
+print ("Different =",Different)
+print ("Multiply =",Multiply)
+print ("Divide =",Divide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
